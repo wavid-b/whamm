909e1c1f7942d70d4696842e0ed1c69bd44938aa
Wavid Bowman <wav.dimportant@gmail.com>
2024-06-10 14:31:34 -0400

    functionality tests init

--- wasm_playground/control_flow/src/main.rs
+++ wasm_playground/control_flow/src/main.rs
@@ -1,12 +1,13 @@
 #[no_mangle]
-pub fn add(a: i32, b: i32) -> i32 {
+pub fn add(mut a: i32, b: i32) -> i32 {
+    a = a+1;
     let res = a + b;
 
     return res;
 }
 
 fn main() {
-    let a = 0;
+    let mut a = 0;
     let mut b = 3;
     let c = add(a, b);
 
